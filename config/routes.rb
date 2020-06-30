179090218bfe59614e556c8797d52d52de6d4135
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-30 19:01:14 +0600

    Make links buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
-  get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
   get 'logout', to: 'sessions#destroy', as: 'logout'
