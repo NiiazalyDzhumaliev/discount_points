1a2e213901b3d8b0b37f9f0e3a6593ac8efece7b
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-30 15:58:43 +0600

    Add a logout feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :logged_in?
     
-    def current_user    
+    def current_user
         User.find_by(id: session[:user_id])  
     end
 
