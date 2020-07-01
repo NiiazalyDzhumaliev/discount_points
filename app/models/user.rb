5454a07f81d8529077089ca1edf68dd49560c854
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-01 14:00:01 +0600

    Add association between user and transactions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+    has_many :transactions, foreign_key: 'author_id', class_name: 'Transaction'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_124756) do
+ActiveRecord::Schema.define(version: 2020_07_01_075903) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_06_30_124756) do
     t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id"
   end
 
   create_table "users", force: :cascade do |t|
