c4efadf1fa49ba72708e01835fbaf2daccccf75a
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-06 21:50:51 +0600

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'sessions#welcome'
   resources :users
   get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-  root to: 'sessions#welcome'
+  get 'logout', to: 'sessions#destroy', as: 'logout'  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
